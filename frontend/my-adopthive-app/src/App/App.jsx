dde053493a3b4b69c93957da7e15843b0e66031e
Iyanuoluwa Abolade Fagbamila <iyanuoluwa@meta.com>
2024-08-04 23:20:36 -0700

    Chat-Update

--- frontend/my-adopthive-app/src/App/App.jsx
+++ frontend/my-adopthive-app/src/App/App.jsx
@@ -1,3 +1,6 @@
+
+import { useContext, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer.jsx';
 import SignIn from '../SignIn/SignIn.jsx';
@@ -9,21 +12,32 @@ import AdopteeList from '../AdopteeList/AdopteeList.jsx';
 import AdopterList from '../AdopterList/AdopterList.jsx';
 import AdopteeHome from '../AdopteeHome/AdopteeHome.jsx';
 import AdopterHome from '../AdopterHome/AdopterHome.jsx';
-import { useContext } from 'react';
-import { UserContext, UserProvider } from '../UserContext';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdopteeFavourites from '../AdopteeFavourites/AdopteeFavourites.jsx';
 import AdopterFavourites from '../AdopterFavourites/AdopterFavourites.jsx';
 import AdopterTopMatches from '../AdopterTopMatches/AdopterTopMatches.jsx';
 import AdopteeTopMatches from '../AdopteeTopMatches/AdopteeTopMatches.jsx';
+import { UserContext, UserProvider } from '../UserContext';
+import { SocketProvider } from '../SocketContext';
+import Chat from '../Chat/Chat.jsx';
 
 function App() {
   const { user, role } = useContext(UserContext);
+  const [isChatVisible, setIsChatVisible] = useState(false);
+  const [receiverId, setReceiverId] = useState(null);
+
+  const handleReceiverId = (id) => {
+    setReceiverId(id);
+    setIsChatVisible(true)
+  };
+
+  const handleChatToggle = () => {
+    setIsChatVisible(!isChatVisible);
+  };
 
   return (
     <div className="App" style={{ overflowX: "hidden" }}>
       <Router>
-      <Header />
+        <Header onChatToggle={handleChatToggle} />
         <Routes>
           <Route path="/mainpage" element={user ? <MainPage /> : <Navigate to="/signin" />} />
           <Route path="/signin" element={<SignIn />} />
@@ -33,13 +47,14 @@ function App() {
           <Route path="/adoptee-profile" element={user ? <AdopteeProfile /> : <Navigate to="/signin" />} />
           <Route path="/adoptee-list" element={user && role === 'Adopter' ? <AdopteeList adopterId={user.id} /> : <Navigate to="/signin" />} />
           <Route path="/adopter-list" element={user && role === 'Adoptee' ? <AdopterList adopteeId={user.id} /> : <Navigate to="/signin" />} />
-          <Route path="/adopter-home" element={user && role === 'Adopter' ? <AdopterHome /> : <Navigate to="/signin" />} />
+          <Route path="/adopter-home" element={user && role === 'Adopter' ? <AdopterHome setReceiverId={handleReceiverId} /> : <Navigate to="/signin" />} />
           <Route path="/adoptee-home" element={user && role === 'Adoptee' ? <AdopteeHome /> : <Navigate to="/signin" />} />
           <Route path="/adoptee-favourites" element={user && role === 'Adoptee' ? <AdopteeFavourites adopteeId={user.id} /> : <Navigate to="/signin" />} />
           <Route path="/adopter-favourites" element={user && role === 'Adopter' ? <AdopterFavourites adopterId={user.id} /> : <Navigate to="/signin" />} />
           <Route path="/adoptee-top-match" element={user && role === 'Adoptee' ? <AdopteeTopMatches adopterId={user.id} /> : <Navigate to="/signin" />} />
           <Route path="/adopter-top-match" element={user && role === 'Adopter' ? <AdopterTopMatches adopterId={user.id} /> : <Navigate to="/signin" />} />
         </Routes>
+        {isChatVisible && <Chat receiverId={receiverId} />}
       </Router>
       <Footer />
     </div>
@@ -49,9 +64,12 @@ function App() {
 function WrappedApp() {
   return (
     <UserProvider>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </UserProvider>
   );
 }
 
 export default WrappedApp;
+
